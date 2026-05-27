6c3a6da227a70403eda68736af43972383b673d2
Vald <35408460+gobllin@users.noreply.github.com>
2026-05-27 12:59:08 +0530

    Update repository.c

--- repository.c
+++ repository.c
@@ -6,28 +6,44 @@
 
 int init_repo(const char *path)
 {
- // Creating the main folder
+ /*
+  * Create the main .minigit directory.
+  * This acts as the root folder for storing
+  * repository metadata and objects.
+  */
  if(mkdir(".minigit") != 0)
  {
   perror("Error creating .minigit");
   return -1;
  }
 
- //Creating vaults for files 
+  /*
+   * Create the objects directory.
+   * This folder will store hashed file objects,
+   * similar to Git's object storage.
+   */ 
  if(mkdir(".minigit/objects") != 0)
  {
   perror("Error creating objects directory");
   return -1;
  }
 
-//Creating the bookmarks for the branches
+/*
+ * Create the refs directory.
+ * This will later store branch references
+ * and commit pointers.
+ */
 if(mkdir(".minigit/refs") != 0)
 {
  perror("Error creating refs directory");
  return -1;
 }
 
-//Creating the headfile 
+/*
+ * Create the HEAD file.
+ * HEAD stores information about the
+ * currently active branch.
+ */ 
 FILE *head = fopen(".minigit/HEAD", "w");
 if(head == NULL)
 {
@@ -35,8 +51,13 @@ if(head == NULL)
  return -1;
 }
 
-//Setting the defulat branch to master 
+/*
+ * Set the default branch reference.
+ * Similar to how Git points HEAD to
+ * the master/main branch.
+ */ 
 fprintf(head, "ref: refs/heads/master\n");
+ // Close the HEAD file after writing
 fclose(head);
 
 printf("Initialized empty Mini-Git repository in %s\n", path);
